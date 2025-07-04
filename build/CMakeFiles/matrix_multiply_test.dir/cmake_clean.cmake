cc68e01798182c344d2deaf85a1616da22cbdcec
Alex Degtyarev <degtyarevaleksandr2005@gmail.com>
2025-07-04 12:19:04 +0300

    Basic multiply

---
+++ build/CMakeFiles/matrix_multiply_test.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/matrix_multiply_test.dir/main.cpp.o"
+  "CMakeFiles/matrix_multiply_test.dir/main.cpp.o.d"
+  "matrix_multiply_test"
+  "matrix_multiply_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/matrix_multiply_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
